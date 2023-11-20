9cba9fa60cb8605d555baec81dc2598d6d80e1f6
Agustin Reyes <aguzz777@gmail.com>
2023-11-19 20:27:55 -0800

    Initial commit

---
+++ src/main/java/com/exampleproject/demo1/LiveApplication.java
@@ -0,0 +1,14 @@
+package com.exampleproject.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(LiveApplication.class, args);
+	}
+
+}
